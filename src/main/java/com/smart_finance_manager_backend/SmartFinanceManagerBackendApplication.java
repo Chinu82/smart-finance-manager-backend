c4ad1d837ab1ca7448cdb9c4e67f8747887341b1
Chinmaya Sahoo <cinshu39@gmail.com>
2026-07-08 13:23:43 +0530

    project_str_springboot

---
+++ src/main/java/com/smart_finance_manager_backend/SmartFinanceManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.smart_finance_manager_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartFinanceManagerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartFinanceManagerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smart_finance_manager_backend/SmartFinanceManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart_finance_manager_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartFinanceManagerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
